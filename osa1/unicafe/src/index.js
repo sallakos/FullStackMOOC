8e26de732a9576d6454d1c00ba99f450797c69ba
Salla Koskinen <sallakos@mbnet.fi>
2019-10-24 09:31:54 +0300

    unicafe step1 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 
 const Heading = ({ heading }) => <h1>{heading}</h1>
 
-const Button = ({ text }) => <button>{text}</button>
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>{text}</button>
+)
 
 const Statistic = ({ type, value }) => <p>{type} {value}</p>
 
@@ -16,13 +18,13 @@ const App = () => {
     return (
         <div>
             <Heading heading="give feedback" />
-            <Button text="good" />
-            <Button text="neutral" />
-            <Button text="bad" />
+            <Button onClick={() => setGood(good + 1)} text="good" />
+            <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+            <Button onClick={() => setBad(bad + 1)} text="bad" />
             <Heading heading="statistics" />
-            <Statistic type="good" value={0}/>
-            <Statistic type="neutral" value={0}/>
-            <Statistic type="bad" value={0}/>
+            <Statistic type="good" value={good} />
+            <Statistic type="neutral" value={neutral} />
+            <Statistic type="bad" value={bad} />
         </div>
     )
 }
