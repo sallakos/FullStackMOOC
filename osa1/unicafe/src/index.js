9439ac9ab9a84f95a6c9af99f57da5c5d5428365
Salla Koskinen <sallakos@mbnet.fi>
2019-10-24 09:48:32 +0300

    unicafe step3 ja step4

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -9,13 +9,35 @@ const Button = ({ onClick, text }) => (
 
 const Statistic = ({ type, value }) => <p>{type} {value}</p>
 
+const Statistics = ({ good, neutral, bad }) => {
+
+    // Lasketaan yhteen
+    const all = good + neutral + bad
+    // Hyvä = 1, neutraali = 0, huono = -1
+    const averageCount = (good * 1 + bad * (-1)) / all
+
+    if (all === 0) {
+        return ("No feedback given")
+    } else {
+        return (
+            <>
+                <Statistic type="good" value={good} />
+                <Statistic type="neutral" value={neutral} />
+                <Statistic type="bad" value={bad} />
+                <Statistic type="all" value={all} />
+                <Statistic type="average" value={averageCount} />
+                <Statistic type="positive" value={(good / all) * 100 + " %"} />
+            </>
+        )
+    }
+
+}
+
 const App = () => {
     // tallenna napit omaan tilaansa
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const all = good + neutral + bad
-    const averageCount = (good * 1 + bad * (-1)) / all
 
     return (
         <div>
@@ -24,12 +46,7 @@ const App = () => {
             <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
             <Button onClick={() => setBad(bad + 1)} text="bad" />
             <Heading heading="statistics" />
-            <Statistic type="good" value={good} />
-            <Statistic type="neutral" value={neutral} />
-            <Statistic type="bad" value={bad} />
-            <Statistic type="all" value={all} />
-            <Statistic type="average" value={averageCount} />
-            <Statistic type="positive" value={(good / all) * 100 + " %"} />
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
