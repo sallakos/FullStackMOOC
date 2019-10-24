f15e2e0ada08f5e707003513d20b35ec35bc2de2
Salla Koskinen <sallakos@mbnet.fi>
2019-10-24 16:30:09 +0300

    2.1 kurssitiedot step 5 valmis

---
+++ osa2/kurssitiedot/src/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({ parts }) => {
+
+  const rows = () => parts.map(part => <Part key={part.id} part={part} />)
+
+  return (
+    <div>
+      {rows()}
+    </div>
+  )
+
+}
+
+export default Content
