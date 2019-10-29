35b10721a3b9c28450befa3ea78b877674c7c8f0
Salla Koskinen <sallakos@mbnet.fi>
2019-10-29 15:34:33 +0200

    2.8 puhelinluettelo step3 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,12 +3,18 @@ import Content from './components/Content'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -16,17 +22,19 @@ const App = () => {
     event.preventDefault();
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     // Mapataan persons-array pelkäksi nimi-arrayksi.
     if (persons.map((persons) => persons.name).indexOf(newName) !== -1) {
-      alert(newName + " is already added to phonebook")
+      alert(`${newName} is already added to phonebook`)
     } else {
       setPersons(persons.concat(personObject))
     }
 
     setNewName('')
+    setNewNumber('')
 
   }
 
@@ -36,6 +44,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} placeholder='new name' />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} placeholder='new number' />
         </div>
         <div>
           <button type="submit">add</button>
--- osa2/puhelinluettelo/src/components/Person.js
+++ osa2/puhelinluettelo/src/components/Person.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const Person = ({ person }) => <>{person.name}<br /></>
+const Person = ({ person }) => <>{person.name} {person.number}<br /></>
 
 export default Person
