449a214f4fbe96ea1a4d41e981b5f00553e94382
Salla Koskinen <sallakos@mbnet.fi>
2019-11-04 14:54:02 +0200

    2.15 puhelinluettelo step7 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Content from './components/Content'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
+import contactService from './services/contacts'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -10,16 +10,13 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
 
-  // Luodaan hook. Haetaan data palvelimelta.
-  const hook = () => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+  useEffect(() => {
+    contactService
+      .getAll()
+      .then(initialContacts => {
+        setPersons(initialContacts)
       })
-  }
-
-  useEffect(hook, [])
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -42,15 +39,22 @@ const App = () => {
       number: newNumber
     }
 
-    // Mapataan persons-array pelkäksi nimi-arrayksi.
-    if (persons.map((persons) => persons.name).indexOf(newName) !== -1) {
-      alert(`${newName} is already added to phonebook`)
-    } else {
-      setPersons(persons.concat(personObject))
-    }
+    contactService
 
-    setNewName('')
-    setNewNumber('')
+      .create(personObject)
+      .then(returnedPerson => {
+
+        // Mapataan persons-array pelkäksi nimi-arrayksi.
+        if (persons.map((persons) => persons.name).indexOf(newName) !== -1) {
+          alert(`${newName} is already added to phonebook`)
+        } else {
+          setPersons(persons.concat(personObject))
+        }
+
+        setNewName('')
+        setNewNumber('')
+
+      })
 
   }
 
@@ -60,10 +64,10 @@ const App = () => {
       <Filter search={search} handleSearch={handleSearch} />
       <h3>Add a new</h3>
       <PersonForm addPerson={addPerson}
-                  newName={newName}
-                  handleNameChange={handleNameChange}
-                  newNumber={newNumber}
-                  handleNumberChange={handleNumberChange} />
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange} />
       <h3>Numbers</h3>
       <Content people={persons} search={search} />
     </div>
