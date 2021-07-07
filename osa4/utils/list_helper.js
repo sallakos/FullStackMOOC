2fefd9b84b9fa46d08a883f385205b9033e826ba
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-07 14:54:09 +0300

    osa 4.4 ensimmainen yksikkotesti

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) =>
+  blogs.reduce((sum, blog) => sum + (blog.likes || 0), 0)
+
 module.exports = {
   dummy,
+  totalLikes,
 }
