7a1211c9646662b34fd097963101f700696d1d33
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-08 11:07:52 +0300

    osa 4.8 testit lisatty ja get-pyynto refaktoroitu

---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('right amount of blogs is returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
