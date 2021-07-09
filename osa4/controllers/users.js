3e88066ca44fc4a9fe251c285c228daa0cc5dfe5
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-09 13:49:58 +0300

    osa 4.16 salasanan validointi

--- osa4/controllers/users.js
+++ osa4/controllers/users.js
@@ -10,18 +10,24 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  if (body.password.length < 3) {
+    response
+      .status(400)
+      .json({ error: 'password must be at least 3 characters' })
+  } else {
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  })
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    })
 
-  const savedUser = await user.save()
+    const savedUser = await user.save()
 
-  response.json(savedUser)
+    response.json(savedUser)
+  }
 })
 
 module.exports = usersRouter
--- osa4/tests/user_api.test.js
+++ osa4/tests/user_api.test.js
@@ -61,6 +61,29 @@ describe('when there is initially one user at db', () => {
     const usersAtEnd = await helper.usersInDb()
     expect(usersAtEnd).toHaveLength(usersAtStart.length)
   })
+
+  test('creation fails with proper statuscode and message if password is too short', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'aku',
+      name: 'Aku Ankka',
+      password: 'pw',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain(
+      'password must be at least 3 characters'
+    )
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
 })
 
 afterAll(() => {
