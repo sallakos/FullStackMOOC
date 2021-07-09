8d2d4219b2da71678836f9580d6e5608b9ad5c5b
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-09 13:30:37 +0300

    osa 4.15 kayttajien lisaaminen onnistuu

--- osa4/app.js
+++ osa4/app.js
@@ -4,6 +4,7 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -19,6 +20,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 
