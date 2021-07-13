9e7996a61355995a0036c488a17acbf773b64956
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-13 14:42:57 +0300

    osa 4.20 tokenin erottaminen middlewareksi

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -2,15 +2,6 @@ const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const _ = require('lodash')
-
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 })
@@ -20,10 +11,12 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if (!request.token || !decodedToken.id) {
+    return response.status(401).json({
+      error: 'token missing or invalid',
+    })
   }
 
   const user = await User.findById(decodedToken.id)
--- osa4/utils/middleware.js
+++ osa4/utils/middleware.js
@@ -1,5 +1,13 @@
 const logger = require('./logger')
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
+}
+
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
@@ -18,4 +26,5 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   errorHandler,
+  tokenExtractor,
 }
