059d0d2b6f08a879eb5fdec7af421ac5944eb42a
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-15 10:22:04 +0300

    osa 5.7 naytettavat blogitiedot

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -1,8 +1,22 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  return (
+    <div style={{ border: 'solid 2px black', padding: '5px', margin: '5px 0' }}>
+      {blog.title}{' '}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? 'hide' : 'view'}
+      </button>
+      <div style={{ display: showDetails ? 'block' : 'none' }}>
+        <div>{blog.url || null}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
