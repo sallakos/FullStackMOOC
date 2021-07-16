b26c7d4cd3e1dde83c14bfe3c1f42b776bc04568
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-16 13:34:02 +0300

    osa 5.18 kirjautumisen testaaminen

--- osa5/cypress/integration/blog_app.spec.js
+++ osa5/cypress/integration/blog_app.spec.js
@@ -1,6 +1,7 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.addUser()
     cy.visit('http://localhost:3000')
   })
 
@@ -8,5 +9,20 @@ describe('Blog app', function () {
     cy.contains('Login')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.login({ username: 'sale', password: 'sala' })
+      cy.contains('Testi Devaaja logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('sale')
+      cy.get('#password').type('wrong')
+      cy.contains('login').click()
+
+      cy.get('.notification').contains('wrong username or password')
+    })
   })
 })
--- osa5/src/components/Notification.js
+++ osa5/src/components/Notification.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Notification = ({ message, type }) =>
   message ? (
     <div
+      className="notification"
       style={{
         border: `solid 3px ${type === 'error' ? 'red' : 'green'}`,
         padding: '10px',
