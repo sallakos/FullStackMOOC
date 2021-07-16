0e3791c452a1e2346d6655620f9a3cf8d6cab3a3
Salla Koskinen <salla.a.koskinen@mehilainen.fi>
2021-07-16 09:28:20 +0300

    korjattu blogin naytettavat tiedot

--- osa5/src/components/Blog.js
+++ osa5/src/components/Blog.js
@@ -20,7 +20,7 @@ const Blog = ({ loggedUser, blog, handleDelete }) => {
 
   return (
     <div style={{ border: 'solid 2px black', padding: '5px', margin: '5px 0' }}>
-      {title}{' '}
+      {title}, {author}{' '}
       <button onClick={() => setShowDetails(!showDetails)}>
         {showDetails ? 'hide' : 'view'}
       </button>
@@ -29,8 +29,8 @@ const Blog = ({ loggedUser, blog, handleDelete }) => {
         <div>
           likes {blogLikes} <button onClick={handleLike}>like</button>
         </div>
-        <div>{author}</div>
-        {blog.user && loggedUser.username === blog.user.username ? (
+        <div>{user && user.name}</div>
+        {user && loggedUser.username === blog.user.username ? (
           <button onClick={() => handleDelete(blog)}>remove</button>
         ) : null}
       </div>
